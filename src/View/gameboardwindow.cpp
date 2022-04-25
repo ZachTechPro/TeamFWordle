1b88a1299bc379dad6bd1847eb27d1c17aca03e3
mallred3 <mallred3@my.westga.edu>
2022-04-25 14:41:45 -0400

    Fixed bug
    fixed error with segmentation faults occurring on submit

--- src/View/gameboardwindow.cpp
+++ src/View/gameboardwindow.cpp
@@ -99,9 +99,10 @@ void GameBoardWindow::performFirstTimeSetup()
         for (int column = 0; column < 5; column++)
         {
             rowX += 30;
-            Fl_Input* input = this-> wordGrid[gridIndex++];
-            input = new Fl_Input(rowX, rowY, 25, 25);
+            Fl_Input* input = new Fl_Input(rowX, rowY, 25, 25);
             input->maximum_size(1);
+            this-> wordGrid[gridIndex++] = input;
+
 
         }
         rowY += 30;
